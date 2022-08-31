e72dae5f215eae4f8c87baca4f12da026e74090d
alexdiem <alexandra.diem@gmail.com>
2022-08-31 16:23:19 +0200

    outsource methods

--- app.py
+++ app.py
@@ -1,165 +1,81 @@
-from dash import Dash, html, dcc, Input, Output
-from pathlib import Path
+from dash import Dash, html, dcc
+from winequality.winequality import *
 
-import plotly.express as px
-import pandas as pd
-
-
-app = Dash(__name__)
-
-colours = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
 
 markdown_intro = """
-# World Cheese Awards
+# Red Wine quality
 
-This is a dashboard visualising the World Cheese Awards dataset from Kaggle (https://www.kaggle.com/datasets/ericsims/world-cheese-awards-worlds-cheesiest-dataset?resource=download).
+This is a dashboard visualising the red wine quality dataset from Kaggle (https://www.kaggle.com/datasets/uciml/red-wine-quality-cortez-et-al-2009).
 """
 
 
-def load_data():
-    pathname = Path("data")
-    filename = "world_cheese_awards_2021.csv"
-    df = pd.read_csv(pathname / filename, index_col=0)
-    return df
-
-
-def etl(df):
-    df['category'] = df['category'].str.replace("–", "-")
-    df[['category_code', 'category', 'subcategory']] = df['category'].str.split(" - ", expand=True)
-    rating_dict = {"SUPER GOLD": 0, "GOLD": 1, "SILVER": 2, "BRONZE": 3}
-    df = df.replace({"rating": rating_dict})
-    return df
-
-
-def generate_table(df, max_rows=10):
-    cols = df.columns
-    return html.Table(
-        style={'color': colours['text']},
-        children=[
-            html.Thead(
-                html.Tr(
-                    [html.Th(col) for col in cols]
-                )
-            ),
-            html.Tbody(
-                [
-                    html.Tr(
-                        [html.Td(df.iloc[i][col]) for col in cols]
-                    ) for i in range(min(len(df), max_rows))
-                ]
-            )
-        ]
-    )
-
-
-def query_category_code(df, category_code):
-    return df.query("category_code == '{}'".format(category_code))
-
-
-@app.callback(
-    Output('company_histogram_graph', 'figure'),
-    Input('cat_code_dropdown', 'value')
-)
-def render_company_histogram(cat_code):
-    data = query_category_code(df, cat_code)
-
-    fig = px.histogram(data, x='company')
-    fig.update_layout(
-        plot_bgcolor=colours['background'],
-        paper_bgcolor=colours['background'],
-        font_color=colours['text']
-    )
-    return fig
-
-
-@app.callback(
-    Output('country_histogram_graph', 'figure'),
-    Input('cat_code_dropdown', 'value')
-)
-def render_country_histogram(cat_code):
-    data = query_category_code(df, cat_code)
-
-    fig = px.histogram(data, x='country')
-    fig.update_layout(
-        plot_bgcolor=colours['background'],
-        paper_bgcolor=colours['background'],
-        font_color=colours['text']
-    )
-    return fig
-
-
-@app.callback(
-    Output('sunburst_graph', 'figure'),
-    Input('cat_code_dropdown', 'value')
-)
-def render_sunburst(cat_code):
-    data = query_category_code(df, cat_code)
-
-    fig = px.sunburst(data, path=["country", "company", "product_name"], values='rating')
-    fig.update_layout(
-        plot_bgcolor=colours['background'],
-        paper_bgcolor=colours['background'],
-        font_color=colours['text']
-    )
-    return fig
-
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = load_data()
-df = etl(df)
+quality_codes = get_quality_codes(df)
 
-cat_codes = df['category_code'].unique()
 
 app.layout = html.Div(
-    style={'backgroundColor': colours['background']},
     children = [
-        html.Div(
-            children=dcc.Markdown(children=markdown_intro),
-            style={
-                'textAlign': 'center',
-                'color': colours['text']
-            }
-        ),
+        dcc.Markdown(children=markdown_intro),
 
         html.Div(
-            [
-                html.Div(
-                    [
-                        html.Div(
-                            children=[
-                                html.Label('Dropdown'),
-                                dcc.Dropdown(
-                                    cat_codes, 
-                                    '5001',
-                                    id='cat_code_dropdown'
-                                )
-                            ]
-                        )
-                    ], style={'padding': 10, 'flex': 1}
-                ),
-
+            children=[
                 html.Div(
                     [
                         html.H4(
-                            children="Table: World Cheese Awards 2021",
+                            children="Quality distribution",
                             style={
-                                'textAlign': 'center',
-                                'color': colours['text']
+                                'textAlign': 'center'
                             }
                         ),
+                        dcc.Graph(
+                            id="quality_piechart",
+                            figure=render_quality_piechart(df)
+                        )
+                    ],
+                    style={
+                        'width': '30%',
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                ),
+
+                html.Div(
+                    children=[
                         generate_table(df)
-                    ]
+                    ],
+                    style={
+                        'width': '65%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
                 )
-            ], style={'display': 'flex', 'flex-direction': 'row'}
+            ]
         ),
 
-        dcc.Graph(id="company_histogram_graph"),
-
-        dcc.Graph(id="country_histogram_graph"),
-
-        dcc.Graph(id="sunburst_graph")
+        html.Div(
+            children=[
+                html.P(
+                    children=["Choose quality"],
+                    style={
+                        'width': '10%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                ),
+                dcc.Dropdown(
+                    quality_codes, 5, id='quality_dropdown',
+                    style={
+                        'width': '20%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                )
+            ],
+            
+        )
     ]
 )
 
