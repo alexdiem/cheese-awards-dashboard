03598df3ae1a1fe10a26d4e4f16494ced38ffe2b
alexdiem <alexandra.diem@gmail.com>
2022-09-01 10:42:50 +0200

    use dash pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import dash
 from dash import Dash, html, dcc
-from winequality.winequality import *
 
 
 markdown_intro = """
@@ -8,76 +8,16 @@ markdown_intro = """
 This is a dashboard visualising the red wine quality dataset from Kaggle (https://www.kaggle.com/datasets/uciml/red-wine-quality-cortez-et-al-2009).
 """
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-df = load_data()
-quality_codes = get_quality_codes(df)
+app = Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
 
 
-app.layout = html.Div(
-    children = [
-        dcc.Markdown(children=markdown_intro),
-
-        html.Div(
-            children=[
-                html.Div(
-                    [
-                        html.H4(
-                            children="Quality distribution",
-                            style={
-                                'textAlign': 'center'
-                            }
-                        ),
-                        dcc.Graph(
-                            id="quality_piechart",
-                            figure=render_quality_piechart(df)
-                        )
-                    ],
-                    style={
-                        'width': '30%',
-                        'display': 'inline-block',
-                        'vertical-align': 'top'
-                    }
-                ),
-
-                html.Div(
-                    children=[
-                        generate_table(df)
-                    ],
-                    style={
-                        'width': '65%', 
-                        'display': 'inline-block',
-                        'vertical-align': 'top'
-                    }
-                )
-            ]
-        ),
-
-        html.Div(
-            children=[
-                html.P(
-                    children=["Choose quality"],
-                    style={
-                        'width': '10%', 
-                        'display': 'inline-block',
-                        'vertical-align': 'top'
-                    }
-                ),
-                dcc.Dropdown(
-                    quality_codes, 5, id='quality_dropdown',
-                    style={
-                        'width': '20%', 
-                        'display': 'inline-block',
-                        'vertical-align': 'top'
-                    }
-                )
-            ],
-            
-        )
-    ]
-)
+app.layout = html.Div(children=[
+    dcc.Markdown(
+        children=[markdown_intro]
+    ),
+    dash.page_container
+])
 
 
 if __name__ == '__main__':
---
+++ pages/dashboard.py
@@ -0,0 +1,120 @@
+import dash
+from dash import callback, dcc, html, Input, Output
+from pathlib import Path
+
+import plotly.express as px
+import pandas as pd
+
+
+dash.register_page(__name__, path="/")
+
+
+def load_data():
+    pathname = Path("data")
+    filename = "winequality-red.csv"
+    df = pd.read_csv(pathname / filename)
+    return df
+
+
+def get_attributes(df):
+    return df.columns
+
+
+def generate_table(max_rows=10):
+    cols = df.columns
+    return html.Table(
+        children=[
+            html.Thead(
+                html.Tr(
+                    [html.Th(col) for col in cols]
+                )
+            ),
+            html.Tbody(
+                [
+                    html.Tr(
+                        [html.Td(df.iloc[i][col]) for col in cols]
+                    ) for i in range(min(len(df), max_rows))
+                ]
+            )
+        ]
+    )
+
+
+def render_quality_piechart():
+    fig = px.pie(df, names='quality')
+    return fig
+
+
+@callback(
+    Output('attribute_histogram', 'figure'),
+    Input('attribute_dropdown', 'value')
+)
+def render_attribute_histogram(attribute):
+    fig = px.histogram(df, x=attribute, color='quality')
+    return fig
+
+
+df = load_data()
+attributes = get_attributes(df)
+
+
+layout = html.Div(
+    children = [
+        html.Div(
+            children=[
+                html.Div(
+                    [
+                        html.H4(
+                            children="Quality distribution",
+                            style={
+                                'textAlign': 'center'
+                            }
+                        ),
+                        dcc.Graph(figure=render_quality_piechart())
+                    ],
+                    style={
+                        'width': '30%',
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                ),
+
+                html.Div(
+                    children=[generate_table()],
+                    style={
+                        'width': '65%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                )
+            ]
+        ),
+
+        html.Div(
+            children=[
+                html.P(
+                    children=["Choose quality"],
+                    style={
+                        'width': '10%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                ),
+                dcc.Dropdown(
+                    attributes, 'fixed acidity', id='attribute_dropdown',
+                    style={
+                        'width': '30%', 
+                        'display': 'inline-block',
+                        'vertical-align': 'top'
+                    }
+                )
+            ]
+        ),
+
+        html.Div(
+            children=[
+                dcc.Graph(id="attribute_histogram")
+            ]
+        )
+    ]
+)
